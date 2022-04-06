0617e6552fcebb909a82aab2f141452c8f666ff1
Azonos712 <nekoooolay@mail.ru>
2022-04-06 19:34:38 +0300

    Add script for async load scenes, design load screen and link it together

---
+++ Assets/Scripts/Scenes/AsyncSceneLoader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsyncSceneLoader : MonoBehaviour
+{
+    [SerializeField] private Image _progressBar;
+    [SerializeField] private GameObject _loaderCanvas;
+    private WaitForSeconds _sleep = new WaitForSeconds(0.01f);
+
+    public void LoadLevel(int index)
+    {
+        StartCoroutine(LoadLevelCoroutine(index));
+    }
+
+    private IEnumerator LoadLevelCoroutine(int index)
+    {
+        _progressBar.fillAmount = 0;
+        _loaderCanvas.SetActive(true);
+
+        var scene = SceneManager.LoadSceneAsync(index);
+        scene.allowSceneActivation = false;
+
+        while (scene.progress != 0.9f)
+        {
+            _progressBar.fillAmount = scene.progress;
+            yield return _sleep;
+        }
+
+        _progressBar.fillAmount = scene.progress;
+        scene.allowSceneActivation = true;
+        //_loaderCanvas.SetActive(false);
+    }
+}
