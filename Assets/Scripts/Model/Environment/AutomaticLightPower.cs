7ca123b04c1fa171f5fbfcbaf24f1c09f11f5848
Azonos712 <nekoooolay@mail.ru>
2022-04-16 15:46:01 +0300

    Add light to store buildings

--- Assets/Scripts/Model/Environment/AutomaticLightPower.cs
+++ Assets/Scripts/Model/Environment/AutomaticLightPower.cs
@@ -1,25 +1,24 @@
-using UnityEditor;
 using UnityEngine;
 
-//[ExecuteInEditMode]
 public class AutomaticLightPower : MonoBehaviour
 {
     private Light _light;
     private DayNightCycle _dayNightCycle;
     private float _timer;
+
     void Start()
     {
-        _light = GetComponent<Light>();
         _dayNightCycle = FindObjectOfType<DayNightCycle>();
+        _light = GetComponent<Light>();
     }
 
     void Update()
     {
-        //ЧТО СДЕЛАТЬ СО СВЕТОМ КОТОРЫЙ ЛАГАЕТ КОГДА ОБНОВЛЯЕТСЯ
+        //Увеличил время обновления, фризы исчезли
         if (_timer <= 0)
         {
             _light.intensity = 1 - _dayNightCycle.Sun.intensity;
-            _timer = 1;
+            _timer = Random.Range(5,31);
         }
 
         _timer -= Time.deltaTime;
